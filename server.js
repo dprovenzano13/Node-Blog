5c9ea9ac15e8e4ce221115f4d442964a21659d34
David Provenzano <dprovenzano13@gmail.com>
2022-05-24 23:26:01 -0400

    Redirect about page

--- server.js
+++ server.js
@@ -1,17 +1,57 @@
 const http = require('http')
+const fs = require('fs')
+
+// const server = http.createServer( (req, res) => {
+//     console.log(req.url, req.method)
+
+//     // set header content type
+//     res.setHeader('Content-Type', 'text/html')
+//     // write the content we want to send to the browser
+//     res.write('<p>hello, world!</p>')
+//     res.write('<p>hello again, world!</p>')
+//     // end the res (response) which will send to the browser
+//     res.end()
+// })
 
 const server = http.createServer( (req, res) => {
     console.log(req.url, req.method)
-
     // set header content type
     res.setHeader('Content-Type', 'text/html')
-    // write the content we want to send to the browser
-    res.write('<p>hello, world!</p>')
-    res.write('<p>hello again, world!</p>')
-    // end the res (response) which will send to the browser
-    res.end()
-})
+    
+// figure out what url the user has visited
+    let path = './views/'
+    switch(req.url) {
+        case '/' :
+            path += 'index.html'
+            res.statusCode = 200
+            break
+        case '/about' :
+            path += 'about.html'
+            res.statusCode = 200
+            break
+        case '/about-me' :
+            res.statusCode = 301
+            res.setHeader('Location', '/about')
+            res.end()
+            break
+        default:
+            path += '404.html'
+            res.statusCode = 404
+            break
 
+    }
+
+    // set an html file
+    fs.readFile(path, (err, data) => {
+        if (err){
+            console.log(err)
+            res.end()
+        } else {
+            res.write(data)
+            res.end()
+        }
+    })
+})
 server.listen(3000, 'localhost', () => {
     console.log('listening for requests on port 3000')
 })
