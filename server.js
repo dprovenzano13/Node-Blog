05346e2727a8928139553672da550a80e231b022
David Provenzano <dprovenzano13@gmail.com>
2022-05-24 22:57:43 -0400

    Add html file

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require('http')
 
 const server = http.createServer( (req, res) => {
-    console.log('request made')
+    console.log(req.url, req.method)
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/html')
+    // write the content we want to send to the browser
+    res.write('<p>hello, world!</p>')
+    res.write('<p>hello again, world!</p>')
+    // end the res (response) which will send to the browser
+    res.end()
 })
 
 server.listen(3000, 'localhost', () => {
